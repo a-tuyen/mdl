c9c1bd6a4c4109e417dfe812fc50224cb9600a49
Amanda Tuyen <atuyen26@gmail.com>
2022-07-14 15:56:41 -0700

    add in nav logo

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -5,6 +5,7 @@ import '../styles/Nav.scss';
 const Nav = () => {
     return (
         <>
+            <img src='https://user-images.githubusercontent.com/77664153/179105100-2197e543-b658-4d5b-b298-33a5423b9465.png'></img>
             <h1>METAL DISTRIBUTORS LTD</h1>
             <p>Home</p>
             <p>About Us</p>
