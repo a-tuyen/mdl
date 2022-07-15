7aa23afa0c84f576036501eaaebf79a5b48a41a1
Amanda Tuyen <atuyen26@gmail.com>
2022-07-15 11:40:56 -0700

    fixes styling for navbar

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -5,12 +5,20 @@ import '../styles/Nav.scss';
 const Nav = () => {
     return (
         <>
-            <img src='https://user-images.githubusercontent.com/77664153/179105100-2197e543-b658-4d5b-b298-33a5423b9465.png'></img>
-            <h1>METAL DISTRIBUTORS LTD</h1>
-            <p>Home</p>
-            <p>About Us</p>
-            <p>Products</p>
-            <p>Contact Us</p>
+            <div className='Nav'>
+                <div className='logo'>
+                    <img src='https://user-images.githubusercontent.com/77664153/179275176-1d25fcfe-6d5e-478d-a1b2-2b461e6da938.png'></img>
+                    <h1 className='title'>METAL DISTRIBUTORS LTD</h1>
+                </div>
+                <div className='buttonMenu'>
+                    <p className='button1'>Home</p>
+                    <p className='button1'>About Us</p>
+                    <p className='button1'>Products</p>
+                    <p className='button1'>Contact Us</p>
+                </div>
+            </div>
+        
+            <hr></hr>
     
     
         </>
