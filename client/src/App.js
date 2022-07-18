06ad4a6f290aef0feb90086b9bc1852acd0b8cd6
Amanda Tuyen <atuyen26@gmail.com>
2022-07-18 15:20:48 -0700

    installs react-router-dom and starts setting it up

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import './App.scss';
 import Nav from './components/Nav';
 import HomePage from './pages/HomePage';
+import { Link } from 'react-router-dom';
 
 const App =() => {
   return (
     <div className="App">
       <Nav/>
+      <Link to ='/products'>Products</Link>
       <HomePage/>
     </div>
   );
