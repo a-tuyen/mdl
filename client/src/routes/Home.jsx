8eff8f4b2ef8fa204989bac8fe2b8cc840eb08fb
Amanda Tuyen <atuyen26@gmail.com>
2022-07-19 15:39:20 -0700

    works on layout of home

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -7,14 +7,16 @@ const Home =() => {
   return (
     <div className='Home'>
       <div className='paragraph'>
-      <p>​​Metal Distributors is a leading non-ferrous metal manufacturer and precision machining company serving a variety of different industries including the marine, nuclear, plumbing, construction and medical industries.</p>
-      <p>We specialize in lead and lead-based products such as Sheet Lead and Lead Bricks for radiation shielding, Lead Anodes for electroplating and Lead Sash Weights for counter balances. We also offer our Reliance brand of sacrificial anodes​ and custom die casting and CNC services. View our full lineup of products here.</p>
-      <p>Our technicians are knowledgeable in all aspects of metalwork, from design and pouring to metal finishing and coating. We can help you find a solution to your metal needs.</p>
+        <p>​​Metal Distributors is a leading non-ferrous metal manufacturer and precision machining company serving a variety of different industries including the marine, nuclear, plumbing, construction and medical industries.</p>
+        <p>We specialize in lead and lead-based products such as Sheet Lead and Lead Bricks for radiation shielding, Lead Anodes for electroplating and Lead Sash Weights for counter balances. We also offer our Reliance brand of sacrificial anodes​ and custom die casting and CNC services. View our full lineup of products here.</p>
+        <p>Our technicians are knowledgeable in all aspects of metalwork, from design and pouring to metal finishing and coating. We can help you find a solution to your metal needs.</p>
       </div>
       <h1>Our Brands and Products</h1>
-      <h2>Lead</h2>
-      <h2>Anodes</h2>
-      <h2>CNC Machining</h2>
+      <div className='Brands'>
+        <h2>Lead</h2>
+        <h2>Anodes</h2>
+        <h2>CNC Machining</h2>
+      </div>
     </div>
 
   );
