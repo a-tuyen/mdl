cda9b08625711b3f6092d670b5374888a404744b
Amanda Tuyen <atuyen26@gmail.com>
2022-07-25 11:05:22 -0700

    add in charts for BRICKS, PIPETUBE, and SHEETLEAD

--- client/src/routes/PipeTube.jsx
+++ client/src/routes/PipeTube.jsx
@@ -5,9 +5,263 @@ const PipeTube = () => {
             <h2>USED IN CHEMICAL PLANTS, NUCLEAR, HYDRO AND PLATING APPLICATIONS</h2>
             <p>Produced in our hydraulic extruder in long lengths, 8 feet is our standard length. We can produce pipe, sleeving and tubing in a variety of diameters, wall thicknesses and weights.</p>
             <p>Below are our standard sizes. We can also do custom sizes upon request.</p>
+            <table>
+                <tr>
+                    <th>INSIDE DIAMETER</th>
+                    <th>WALL THICKNESS</th>
+                    <th>OUTSIDE DIAMETER</th>
+                    <th>WEIGHT (lbs/ft)</th>
+                </tr>
+                <tr>
+                    <td rowSpan={'3'}>¾"</td>
+                    <td>1/8"</td>
+                    <td>​1"</td>
+                    <td>​​1.69</td>
+                </tr>
+                <tr><td>3/16"</td>
+                    <td>1 1/8"</td>
+                    <td>2.72</td>
+                </tr>
+                <tr>
+                    <td>1/4"</td>
+                    <td>1 1/4"</td>
+                    <td>3.87</td>
+                </tr>
+                <tr>
+                    <td rowSpan={'3'}>1"</td>
+                    <td>1/8"</td>
+                    <td>1 1/4"</td>
+                    <td>2.17</td>
+                </tr>
+                <tr>
+                    <td>3/16"</td>
+                    <td>1 3/8"</td>
+                    <td>3.44</td>
+                </tr>
+                <tr>
+                    <td>1/4"</td>
+                    <td>1 1/2"</td>
+                    <td>4.83</td>
+                </tr>
+                <tr>
+                    <td rowSpan={'3'}>1 ¼"</td>
+                    <td>1/8"</td>
+                    <td> 1 1/2"</td>
+                    <td>2.66</td>
+                </tr>
+                <tr>
+                    <td>3/16"</td>
+                    <td>1 5/8"</td>
+                    <td>4.17</td>
+                </tr>
+                <tr>
+                    <td>1/4"</td>
+                    <td>1 3/4"</td>
+                    <td>5.8</td>
+                </tr>
+                <tr>
+                    <td rowSpan={'3'}>1 ½"</td>
+                    <td>1/8"</td>
+                    <td>1 3/4"</td>
+                    <td>3.14</td>
+                </tr>
+                <tr>
+                    <td>3/16"</td>
+                    <td>1 7/8"</td>
+                    <td>4.89</td>
+                </tr>
+                <tr><td>1/4"</td>
+                    <td>2"</td>
+                    <td>6.77</td>
+                </tr>
+                <tr>
+                    <td rowSpan={'3'}>1 ¾"</td>
+                    <td>1/8"</td>
+                    <td>2"</td>
+                    <td>3.62</td>
+                </tr>
+                <tr>
+                    <td>3/16"</td>
+                    <td>2 1/8"</td>
+                    <td>5.62</td>
+                </tr>
+                <tr><td>1/4"</td>
+                    <td>2 1/4"</td>
+                    <td>7.73</td>
+                </tr>
+                <tr>
+                    <td rowSpan={'3'}>2"</td>
+                    <td>1/8"</td>
+                    <td>2"</td>
+                    <td>4.11</td>
+                </tr>
+                <tr>
+                    <td>3/16"</td>
+                    <td>2 3/8"</td>
+                    <td>6.34</td>
+                </tr>
+                <tr>
+                    <td>1/4"</td>
+                    <td>2 1/2"</td>
+                    <td>8.7</td>
+                </tr>
+                <tr>
+                    <td rowSpan={'3'}>2 ½"</td>
+                    <td>1/8"</td>
+                    <td>2 3/4"</td>
+                    <td>5.07</td>
+                </tr>
+                <tr>
+                    <td>3/16"</td>
+                    <td>2 7/8"</td>
+                    <td>7.79</td>
+                </tr>
+                <tr>
+                    <td>1/4"</td>
+                    <td>3"</td>
+                    <td>10.63</td>
+                </tr>
+                <tr>
+                    <td rowSpan={'3'}>3"</td>
+                    <td>1/8"</td>
+                    <td>3 1/4"</td>
+                    <td>6.04</td>
+                </tr>
+                <tr>
+                    <td>3/16"</td>
+                    <td>3 3/8"</td>
+                    <td>9.24</td>
+                </tr>
+                <tr>
+                    <td>1/4"</td>
+                    <td>3 1/2"</td>
+                    <td>12.56</td>
+                </tr>
+                <tr>
+                    <td rowSpan={'3'}>3 ½"</td>
+                    <td>1/8"</td>
+                    <td>3 3/4"</td>
+                    <td>7.01</td>
+                </tr>
+                <tr><td>3/16"</td>
+                    <td>3 7/8"</td>
+                    <td>10.69</td>
+                </tr>
+                <tr><td>1/4"</td>
+                    <td>4"</td>
+                    <td>14.5</td>
+                </tr>
+                <tr><td rowSpan={'3'}>4"</td>
+                    <td>1/8"</td>
+                    <td>4 1/4"</td>
+                    <td>7.97</td>
+                </tr>
+                <tr>
+                    <td>3/16"</td>
+                    <td>4 3/8"</td>
+                    <td>12.14</td>
+                </tr>
+                <tr>
+                    <td>1/4"</td>
+                    <td>4 1/2"</td>
+                    <td>16.43</td>
+                </tr>
+                <tr>
+                    <td rowSpan={'3'}>4 ½"</td>
+                    <td>1/8"</td>
+                    <td>4 3/4"</td>
+                    <td>8.94</td>
+                </tr>
+                <tr>
+                    <td>3/16"</td>
+                    <td>4 7/8"</td>
+                    <td>13.59</td>
+                </tr>
+                <tr>
+                    <td>1/4"</td>
+                    <td>5"</td>
+                    <td>18.36</td>
+                </tr>
+                <tr>
+                    <td rowSpan={'3'}>5"</td>
+                    <td>1/8"</td>
+                    <td>5 1/4"</td>
+                    <td>9.91</td>
+                </tr>
+                <tr>
+                    <td>3/16"</td>
+                    <td>5 3/8"</td>
+                    <td>15.04</td>
+                </tr>
+                <tr>
+                    <td>1/4"</td>
+                    <td>5 1/2"</td>
+                    <td>20.3</td>
+                </tr>
+                <tr>
+                    <td rowSpan={'3'}>6"</td>
+                    <td>1/8"</td>
+                    <td>6 1/4"</td>
+                    <td>11.84</td>
+                </tr>
+                <tr>
+                    <td>3/16"</td>
+                    <td>6 3/8"</td>
+                    <td>17.94</td>
+                </tr>
+                <tr>
+                    <td>1/4"</td>
+                    <td>6 1/2"</td>
+                    <td>24.16</td>
+                </tr>
+                <tr><td rowSpan={'3'}>7"</td>
+                    <td>1/8"</td>
+                    <td>7 1/4"</td>
+                    <td>13.77</td>
+                </tr>
+                <tr>
+                    <td>3/16"</td>
+                    <td>7 1/8"</td>
+                    <td>20.84</td>
+                </tr>
+                <tr>
+                    <td>1/4"</td>
+                    <td>7 1/4"</td>
+                    <td>28.03</td>
+                </tr>
+                <tr><td rowSpan={'3'}>8"</td>
+                    <td>1/8"</td>
+                    <td>8 1/4"</td>
+                    <td>15.71</td>
+                </tr>
+                <tr>
+                    <td>3/16"</td>
+                    <td>8 3/8"</td>
+                    <td>23.74</td>
+                </tr>
+                <tr>
+                    <td>1/4"</td>
+                    <td>8 1/2"</td>
+                    <td>31.89</td>
+                </tr>
+                <tr>
+                    <td rowSpan={'3'}>10"</td>
+                    <td>1/8"</td>
+                    <td>10 1/4"</td>
+                    <td>19.57</td>
+                </tr>
+                <tr><td>3/16"</td>
+                    <td>10 3/8"</td>
+                    <td>29.54</td>
+                </tr>
+                <tr><td>1/4"</td>
+                    <td>10 1/2"</td>
+                    <td>39.63</td>
+                </tr>
+            </table>
         </div>
     );
-    
+
 };
 
 export default PipeTube;
--- client/src/routes/SheetLead.jsx
+++ client/src/routes/SheetLead.jsx
@@ -5,6 +5,131 @@ const SheetLead = () => {
             <h2>COMMONLY USED FOR RADIATION SHIELDING AND SOUND PROOFING</h2>
             <p>Manufactured from refined pure leads that conforms to ASTM B-29, B-749 and Federal QQ-L-201F, QQ-L-171E specifications for lead.</p>
             <p>Our sheet lead is produced in-house by our skilled technicians on one of the most precise Lead Rolling Mills in the world today. Thickness can range from just 1/64" to 1" in thickness. We stock standard sizes of sheet lead such as 34" x 84" and 36" x 96" and are able to produce custom sizes upon request.</p>
+            <table>
+                <tr>
+                    <th>SHEET LEAD THICKNESS CHART</th>
+                </tr>
+                <tr>
+                    <th>INCHES (DECIMAL)</th>
+                    <th>INCHES (FRACTION)</th>
+                    <th>APPROX LBS/FT²</th>
+                    <th>METRIC (mm)</th>
+                    <th>APPROX KG/M²</th>
+                </tr>
+                <tr>
+                    <td>0.0156"</td>
+                    <td>1/64"</td>
+                    <td>0.4mm</td>
+                    <td>​​1#</td>
+                    <td>4.88</td>
+                </tr>
+                <tr>
+                    <td>0.0312"</td>
+                    <td>1/32"</td>
+                    <td>0.8mm</td>
+                    <td>2#</td>
+                    <td>9.76</td>
+                </tr>
+                <tr>
+                    <td>0.0391"</td>
+                    <td>5/128"</td>
+                    <td>1.0mm</td>
+                    <td>2½#</td>
+                    <td>12.2</td>
+                </tr>
+                <tr>
+                    <td>0.0468"</td>
+                    <td>3/64"</td>
+                    <td>1.2mm</td>
+                    <td>3#</td>
+                    <td>14.64</td>
+                </tr>
+                <tr>
+                    <td>0.0625"</td>
+                    <td>1/16"</td>
+                    <td>1.6mm</td>
+                    <td>4#</td>
+                    <td>19.52</td>
+                </tr>
+                <tr>
+                    <td>0.0781"</td>
+                    <td>5/64"</td>
+                    <td>2.0mm</td>
+                    <td>5#</td>
+                    <td>24.4</td>
+                </tr>
+                <tr>
+                    <td>0.0937"</td>
+                    <td>3/32"</td>
+                    <td>2.4mm</td>
+                    <td>6#</td>
+                    <td>29.28</td>
+                </tr>
+                <tr>
+                    <td>0.125"</td>
+                    <td>1/8"</td>
+                    <td>3.2mm</td>
+                    <td>8#</td>
+                    <td>39.04</td>
+                </tr>
+                <tr>
+                    <td>0.1563"</td>
+                    <td>5/32"</td>
+                    <td>4.0mm</td>
+                    <td>10#</td>
+                    <td>48.8</td>
+                </tr>
+                <tr>
+                    <td>0.1875"</td>
+                    <td>3/16"</td>
+                    <td>4.8mm</td>
+                    <td>12#</td>
+                    <td>58.56</td>
+                </tr>
+                <tr>
+                    <td>0.2188"</td>
+                    <td>7/32"</td>
+                    <td>5.6mm</td>
+                    <td>14#</td>
+                    <td>68.32</td>
+                </tr>
+                <tr>
+                    <td>0.25"</td>
+                    <td>1/4"</td>
+                    <td>6.4mm</td>
+                    <td>16#</td>
+                    <td>78.08</td>
+                </tr>
+                <tr>
+                    <td>0.375"</td>
+                    <td>3/8"</td>
+                    <td>9.5mm</td>
+                    <td> 22 ½#</td>
+                    <td>109.8</td>
+                </tr>
+                <tr>
+                    <td>0.50"</td>
+                    <td>1/2"</td>
+                    <td>12.7mm</td>
+                    <td>30#</td>
+                    <td>146.4</td>
+                </tr>
+                <tr>
+                    <td>0.75"</td>
+                    <td>3/4"</td>
+                    <td>19.1mm</td>
+                    <td>45#</td>
+                    <td>219.6</td>
+                </tr>
+                <tr>
+                    <td>1"</td>
+                    <td>1"</td>
+                    <td>25.4mm</td>
+                    <td>60#</td>
+                    <td>292.8</td>
+                </tr>
+
+            </table>
         </div>
     );
 };
