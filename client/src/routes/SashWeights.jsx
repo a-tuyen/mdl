67f1d18416aea8fb6434469aacee7b6c71aa2a7d
Amanda Tuyen <atuyen26@gmail.com>
2022-07-25 15:52:52 -0700

    add sash weight picture

--- client/src/routes/SashWeights.jsx
+++ client/src/routes/SashWeights.jsx
@@ -4,6 +4,7 @@ const SashWeights = () => {
             <h1>Sash Weights</h1>
             <h2>USED TO COUNTERBALANCE THE WEIGHT OF A SLIDING SASH IN A SASH WINDOW SO THE WINDOW POSITION CAN BE HELD AT ANY HEIGHT</h2>
             <p>Available in 1.5" x 1.5" square shape. We can cut the sash weights to your desired lengths, up to 6 feet long.</p>
+            <img src='https://user-images.githubusercontent.com/77664153/180884978-06bc0550-e93c-4eb7-9a0f-c3dd8bcdc4de.png'></img>
         </div>
     );
     
