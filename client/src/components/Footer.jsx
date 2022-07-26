ab9b68a15baf0a58688819f851f7014990b7a45f
Amanda Tuyen <atuyen26@gmail.com>
2022-07-26 09:47:06 -0700

    works on Footer and changes website Title

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,10 +2,23 @@
 
 const Footer = () => {
     return (
-        <div>
-
+        <>
             <hr></hr>
-        </div>
+            <h3>HOURS OF OPERATION</h3>
+            <p>MONDAY............8:00am - 3:30pm</p>
+            <p>TUESDAY............8:00am - 3:30pm</p>
+            <p>WEDNESDAY.....8:00am - 3:30pm</p>
+            <p>THURSDAY.........8:00am - 3:30pm</p>
+            <p>FRIDAY...............8:00am - 3:30pm</p>
+            <p>PHONE: 604-420-3137 / 1-877-420-3731 (TOLL FREE)</p>
+            <p>EMAIL: info@metaldist.com</p>
+            <p>FAX: 604-420-9240</p>
+            <p>Metal Distributors LTD</p>
+            <p>7220 Winston St</p>
+            <p>Burnaby, BC  V5A 2G9</p>
+
+
+        </>
     );
 };
 
