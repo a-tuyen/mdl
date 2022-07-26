ffbd75fce92ddb4243867ab435a91b6c972a2ae3
Amanda Tuyen <atuyen26@gmail.com>
2022-07-26 13:37:36 -0700

    footer styling

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,23 +4,34 @@ const Footer = () => {
     return (
         <div className="Footer">
             <hr></hr>
-            <div className='Hours'>
-                <h3>HOURS OF OPERATION</h3>
-                <p>MONDAY............8:00am - 3:30pm</p>
-                <p>TUESDAY............8:00am - 3:30pm</p>
-                <p>WEDNESDAY.....8:00am - 3:30pm</p>
-                <p>THURSDAY.........8:00am - 3:30pm</p>
-                <p>FRIDAY...............8:00am - 3:30pm</p>
-            </div>
-            <div className='Contact'>
-            <p>PHONE: 604-420-3137 / 1-877-420-3731 (TOLL FREE)</p>
-            <p>EMAIL: info@metaldist.com</p>
-            <p>FAX: 604-420-9240</p>
-            </div>
-            <div className='Address'>
-            <p>Metal Distributors LTD</p>
-            <p>7220 Winston St</p>
-            <p>Burnaby, BC  V5A 2G9</p>
+            <div className='Main'>
+                <div className='Hours'>
+                    <h3>HOURS OF OPERATION</h3>
+                    {/* <p>MONDAY.............8:00am - 3:30pm</p>
+                    <p>TUESDAY............8:00am - 3:30pm</p>
+                    <p>WEDNESDAY.....8:00am - 3:30pm</p>
+                    <p>THURSDAY.........8:00am - 3:30pm</p>
+                    <p>FRIDAY...............8:00am - 3:30pm</p>
+                    <p>WEEKENDS...................CLOSED</p> */}
+
+                    <p>MONDAY - FRIDAY.........8:00am - 3:30pm</p>
+                    <p>WEEKENDS................................CLOSED</p>
+
+                </div>
+                <div className='Contact'>
+                    <h3>CONTACT US</h3>
+                    <p>EMAIL: info@metaldist.com</p>
+                    <p>PHONE: 604 - 420 - 3137</p>
+                    <p>TOLL FREE: 1 - 877 - 420 - 3731</p>
+                    <p>FAX: 604 - 420 - 9240</p>
+                </div>
+                <div className='Address'>
+                    <h3>LOCATION</h3>
+                    <p>Metal Distributors LTD</p>
+                    <p>7220 Winston St</p>
+                    <p>Burnaby, BC  V5A 2G9</p>
+                    <p>CANADA</p>
+                </div>
             </div>
 
 
