dfe075263fe49e84b06762b0649232ec16966c20
Amanda Tuyen <atuyen26@gmail.com>
2022-07-27 15:19:32 -0700

    adds state for form

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,18 +1,41 @@
+import axios from "axios";
+import React, { useState } from "react";
+
+
 const Form = () => {
+    const [sent, setSent] = useState(false)
+    const [text, setText] = useState('')
+
+    const handleSend = async () => {
+        setSent(true)
+        try {
+            await axios.post('http://localhost:3000/', {
+                text
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
         <div className="Form">
             <h1> Send us a Message!</h1>
-            <form onSubmit={()=> {}}>
-                <label>Name</label>
-                <input type='text' required />
-                <label>Email</label>
-                <input type='text' required />
-                <label>Subject</label>
-                <input type='text' required />
-                <label>Message</label>
-                <input type='text' required /> 
-                <button type='submit'>Submit</button>
-            </form>
+
+            {!sent ? (
+                <form onSubmit={handleSend}>
+                    <label>Name</label>
+                    <input type='text' value={text} onChange={(e) => setText(e.target.value)} />
+                    <label>Email</label>
+                    <input type='text' required />
+                    <label>Subject</label>
+                    <input type='text' required />
+                    <label>Message</label>
+                    <input type='text' required />
+                    <button type='submit'>Submit</button>
+                </form>
+            ) : (
+                <h1>Email Sent</h1>
+            )}
         </div>
     );
 };
--- client/src/routes/Contact.jsx
+++ client/src/routes/Contact.jsx
@@ -1,3 +1,6 @@
+import axios from 'axios';
+import React, {useState} from 'react'
+
 import Form from "../components/Form";
 
 const Contact = () => {
