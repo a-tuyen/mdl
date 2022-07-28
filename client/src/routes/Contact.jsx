e5f12ddf8ed085aa9d5370bfd4529ea1a0f83248
Amanda Tuyen <atuyen26@gmail.com>
2022-07-28 15:52:38 -0700

    testing out diff contact forms

--- client/src/routes/Contact.jsx
+++ client/src/routes/Contact.jsx
@@ -1,13 +1,19 @@
 import axios from 'axios';
 import React, {useState} from 'react'
 
-import Form from "../components/Form";
+import ContactForm from "../components/ContactForm";
 
 const Contact = () => {
+
+    axios.get('http://localhost:5000/contact')
+        .then(res => {
+            console.log('data has come back');
+            console.log(res.data);
+        })
     return (
         <div className='Contact-us'>
             <h1>Contact Us</h1>
-            <Form />
+            <ContactForm />
         </div>
     );
 };
