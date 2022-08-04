d25e47bfbe3c0e85337ca95aa7d0f8b63f07255d
Amanda Tuyen <atuyen26@gmail.com>
2022-08-04 13:52:54 -0700

    finishes styling of homepage

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -7,23 +7,40 @@ const Home = () => {
   return (
     <div className='Home'>
       <div className='paragraph'>
-      <img src='https://user-images.githubusercontent.com/77664153/180872927-70cd5e4c-b534-49ad-83ce-3ac92fdd11fc.png'></img>
+        <img src='https://user-images.githubusercontent.com/77664153/180872927-70cd5e4c-b534-49ad-83ce-3ac92fdd11fc.png'></img>
         <p>​​Metal Distributors is a leading non-ferrous metal manufacturer and precision machining company serving a variety of different industries including the marine, nuclear, plumbing, construction and medical industries.</p>
         <p>We specialize in lead and lead-based products such as Sheet Lead and Lead Bricks for radiation shielding, Lead Anodes for electroplating and Lead Sash Weights for counter balances. We also offer our Reliance brand of sacrificial anodes​ and custom die casting and CNC services. View our full lineup of products here.</p>
         <p>Our technicians are knowledgeable in all aspects of metalwork, from design and pouring to metal finishing and coating. We can help you find a solution to your metal needs.</p>
       </div>
       <h1>Our Brands and Products</h1>
+      <div className='products'>
+        <div className='section'>
+          <h2>LEAD</h2>
+          <img src="https://user-images.githubusercontent.com/77664153/182916916-76b1cd90-51e3-4917-8ffe-894333f5dc14.png" />
+        </div>
+        <div className='section'>
+          <h2>SACRIFICIAL ANODES</h2>
+          <img src="https://user-images.githubusercontent.com/77664153/182916917-a5437d32-1c0f-4e27-8764-1f1d075e3e51.png" />
+        </div>
+        <div className='section'>
+          <h2>CNC MACHINING</h2>
+          <img src="https://user-images.githubusercontent.com/77664153/182916915-f5d4938f-1a1d-49e7-a59e-273ee64283b9.png" />
+        </div>
+      </div>
       <div className='Brands'>
-        <h2>Lead</h2>
-        <h2>Anodes</h2>
-        <h2>CNC Machining</h2>
-        <img src="https://user-images.githubusercontent.com/77664153/182916916-76b1cd90-51e3-4917-8ffe-894333f5dc14.png"/>
-        <img src="https://user-images.githubusercontent.com/77664153/182916917-a5437d32-1c0f-4e27-8764-1f1d075e3e51.png" />
-        <img src="https://user-images.githubusercontent.com/77664153/182916915-f5d4938f-1a1d-49e7-a59e-273ee64283b9.png" />
+        <div className='titles'>
+
+
+
+        </div>
+        <div className='images'>
+
+
+        </div>
         {/* <img src='https://user-images.githubusercontent.com/77664153/179861454-25ce6c5b-a44e-4783-bb3b-22bb4d8ac0ec.png'></img>
         <img src='https://user-images.githubusercontent.com/77664153/179861617-a0d0c58a-5449-4719-bcc4-e142e635c262.jpg'></img>
         <img src='https://cdn.pixabay.com/photo/2019/08/11/07/44/laser-4398314_960_720.jpg'></img> */}
-        </div>
+      </div>
     </div>
 
   );
