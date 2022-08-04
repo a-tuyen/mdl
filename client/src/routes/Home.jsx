777958c4bbc876eb915443e2c95b47ef02d064ac
Amanda Tuyen <atuyen26@gmail.com>
2022-08-04 10:55:30 -0700

    swap out pictures

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -17,9 +17,12 @@ const Home = () => {
         <h2>Lead</h2>
         <h2>Anodes</h2>
         <h2>CNC Machining</h2>
-        <img src='https://user-images.githubusercontent.com/77664153/179861454-25ce6c5b-a44e-4783-bb3b-22bb4d8ac0ec.png'></img>
+        <img src="https://user-images.githubusercontent.com/77664153/182916916-76b1cd90-51e3-4917-8ffe-894333f5dc14.png"/>
+        <img src="https://user-images.githubusercontent.com/77664153/182916917-a5437d32-1c0f-4e27-8764-1f1d075e3e51.png" />
+        <img src="https://user-images.githubusercontent.com/77664153/182916915-f5d4938f-1a1d-49e7-a59e-273ee64283b9.png" />
+        {/* <img src='https://user-images.githubusercontent.com/77664153/179861454-25ce6c5b-a44e-4783-bb3b-22bb4d8ac0ec.png'></img>
         <img src='https://user-images.githubusercontent.com/77664153/179861617-a0d0c58a-5449-4719-bcc4-e142e635c262.jpg'></img>
-        <img src='https://cdn.pixabay.com/photo/2019/08/11/07/44/laser-4398314_960_720.jpg'></img>
+        <img src='https://cdn.pixabay.com/photo/2019/08/11/07/44/laser-4398314_960_720.jpg'></img> */}
         </div>
     </div>
 
