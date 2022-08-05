6705f9764256350265125b18a477dbf4e37d1f4e
Amanda Tuyen <atuyen26@gmail.com>
2022-08-05 08:22:25 -0700

    add in required component to form

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -4,59 +4,62 @@ import { useState } from "react";
 
 const Form = () => {
 
-    // const [name, setName] = useState('');
-    // const [email, setEmail] = useState('');
-    // const [message, setMessage] = useState('');
- 
-    const [inputs, setInputs] = useState({});
+  // const [name, setName] = useState('');
+  // const [email, setEmail] = useState('');
+  // const [message, setMessage] = useState('');
 
-    const handleChange = (event) => {
-      const name = event.target.name;
-      const value = event.target.value;
-      setInputs(values => ({...values, [name]: value}))
-    }
+  const [inputs, setInputs] = useState({});
 
-    const onFormSubmit = (evt) => {
-        evt.preventDefault();
-        console.log(inputs);
-        // console.log('NAME:', name);
-        // console.log('message:', message);
-        // console.log('email: ', email);
-        // return axios.post('http://localhost:3000/contact', {message, name})
-        // console.log('event ', evt.target.elements)
-        
-    }
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({ ...values, [name]: value }))
+  }
 
-    return (
-        <div className="Form">
-                <form onSubmit={onFormSubmit}>
-      <label>Enter your name:
-      <input 
-        type="text" 
-        name="name" 
-        value={inputs.username || ""} 
-        onChange={handleChange}
-      />
-      </label>
-      <label>Email:
-        <input 
-          type="email" 
-          name="email" 
-          value={inputs.email || ""} 
-          onChange={handleChange}
-        />
+  const onFormSubmit = (evt) => {
+    evt.preventDefault();
+    console.log(inputs);
+    // console.log('NAME:', name);
+    // console.log('message:', message);
+    // console.log('email: ', email);
+    // return axios.post('http://localhost:3000/contact', {message, name})
+    // console.log('event ', evt.target.elements)
+
+  }
+
+  return (
+    <div className="Form">
+      <form onSubmit={onFormSubmit}>
+        <label>Name:
+          <input
+            type="text"
+            name="name"
+            value={inputs.name || ""}
+            onChange={handleChange}
+            required="required"
+          />
+        </label>
+        <label>Email:
+          <input
+            type="email"
+            name="email"
+            value={inputs.email || ""}
+            onChange={handleChange}
+            required="required"
+          />
         </label>
         <label>Message:
-        <input 
-          type="text" 
-          name="message" 
-          value={inputs.message || ""} 
-          onChange={handleChange}
-        />
+          <input
+            type="text"
+            name="message"
+            value={inputs.message || ""}
+            onChange={handleChange}
+            required="required"
+          />
         </label>
         <input type="submit" />
-    </form>
-            {/* <h1> Send us a Message!</h1>
+      </form>
+      {/* <h1> Send us a Message!</h1>
                 <form onSubmit={onFormSubmit}>
                     <label>Name</label>
                     <input type='text' required name='name' value={name} onChange={(evt) => setName(evt.target.value)} />
@@ -67,8 +70,8 @@ const Form = () => {
                     <button type='submit'>Submit</button>
                 </form>
                 <h1>Email Sent</h1> */}
-        </div>
-    );
+    </div>
+  );
 };
 
 export default Form;
