dedbe8359fa6c6c7a1f93eb6b9b20b22daff38c5
Amanda Tuyen <atuyen26@gmail.com>
2022-08-10 15:55:20 -0700

    continues work on products page styling

--- client/src/routes/Products.jsx
+++ client/src/routes/Products.jsx
@@ -1,13 +1,17 @@
 import { Outlet } from 'react-router-dom';
 import SideBar from '../components/SideBar';
+import '../styles/Products.scss'
 
-const Products =() => {
-    return (
-      <div className="Products">
-          <h1>Products</h1>
-          <SideBar/>
-      </div>
-    );
-  };
-  
-  export default Products;
+const Products = () => {
+  return (
+    <div className="Products">
+      <section>
+      <SideBar />
+      </section>
+      <h1 className='Title'>Products</h1>
+
+    </div>
+  );
+};
+
+export default Products;
