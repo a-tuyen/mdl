c0c11e2bd43e8b693f4ce8557ef28370cb68851a
Amanda Tuyen <atuyen26@gmail.com>
2022-08-12 10:44:38 -0700

    lead anodes page styling complete

--- client/src/routes/LeadAnodes.jsx
+++ client/src/routes/LeadAnodes.jsx
@@ -4,19 +4,23 @@ const LeadAnodes = () => {
     return (
         <div className="LeadAnodes">
             <h1>Lead Anodes</h1>
-            <h2>TYPICALLY USED IN ELECTROPLATING PROCESSES TO ALTER THE CHEMICAL AND PHYSICAL PROPERTIES OF METAL SURFACES</h2>
+            <h3>TYPICALLY USED IN ELECTROPLATING PROCESSES TO ALTER THE CHEMICAL AND PHYSICAL PROPERTIES OF METAL SURFACES</h3>
 
-            <h3>We offer a variety of cast or extruded anodes in a wide range of configurations, such as Grids and Baskets. Each of our anodes go through the same process to produce a fine grain structure essential to even plate-out and maximum throwing power of the plating solution.
+            <p>We offer a variety of cast or extruded anodes in a wide range of configurations, such as Grids and Baskets. Each of our anodes go through the same process to produce a fine grain structure essential to even plate-out and maximum throwing power of the plating solution.
                 We stock the following metals to produce any alloy you require:
-            </h3>
-            <ul>
-                <li>Tin (Sn)</li>
-                <li>Lead (Pb)</li>
-                <li>Antimony (Sb)</li>
-                <li>Silver (Ag)</li>
-                <li>Bismuth (Bi)</li>
-                <li>Aluminum (Al)</li>
-            </ul>
+            </p>
+            <div className='list'>
+                <ul>
+                    <li>Tin (Sn)</li>
+                    <li>Lead (Pb)</li>
+                    <li>Antimony (Sb)</li>
+                </ul>
+                <ul>
+                    <li>Silver (Ag)</li>
+                    <li>Bismuth (Bi)</li>
+                    <li>Aluminum (Al)</li>
+                </ul>
+            </div>
 
         </div>
     );
