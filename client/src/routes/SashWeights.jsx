b8bb42666fe89ff2f1ba160db466d3c67558479d
Amanda Tuyen <atuyen26@gmail.com>
2022-08-15 15:14:14 -0700

    finish styling for sash weights

--- client/src/routes/SashWeights.jsx
+++ client/src/routes/SashWeights.jsx
@@ -1,10 +1,12 @@
+import '../styles/ProductsList.scss';
+
 const SashWeights = () => {
     return (
-        <div className='sash-weights'>
+        <div className='main'>
             <h1>Sash Weights</h1>
-            <h2>USED TO COUNTERBALANCE THE WEIGHT OF A SLIDING SASH IN A SASH WINDOW SO THE WINDOW POSITION CAN BE HELD AT ANY HEIGHT</h2>
+            <h3>USED TO COUNTERBALANCE THE WEIGHT OF A SLIDING SASH IN A SASH WINDOW SO THE WINDOW POSITION CAN BE HELD AT ANY HEIGHT</h3>
             <p>Available in 1.5" x 1.5" square shape. We can cut the sash weights to your desired lengths, up to 6 feet long.</p>
-            <img src='https://user-images.githubusercontent.com/77664153/180884978-06bc0550-e93c-4eb7-9a0f-c3dd8bcdc4de.png'></img>
+            <img className='img-sash-weight' src='https://user-images.githubusercontent.com/77664153/180884978-06bc0550-e93c-4eb7-9a0f-c3dd8bcdc4de.png'></img>
         </div>
     );
     
