5ee0ad09fe96bbf422773010a8f537b25ad6b0ac
Amanda Tuyen <atuyen26@gmail.com>
2022-08-22 08:50:58 -0700

    add in link to home page, change size of reliance logo and add underline only to nav hover

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
       <div className='paragraph'>
         <img src='https://user-images.githubusercontent.com/77664153/180872927-70cd5e4c-b534-49ad-83ce-3ac92fdd11fc.png' alt='rolls of sheet lead on a shelf'></img>
         <p>​​Metal Distributors is a leading non-ferrous metal manufacturer and precision machining company serving a variety of different industries including the marine, nuclear, plumbing, construction and medical industries.</p>
-        <p>We specialize in lead and lead-based products such as Sheet Lead and Lead Bricks for radiation shielding, Lead Anodes for electroplating and Lead Sash Weights for counter balances. We also offer our Reliance brand of sacrificial anodes​ and custom die casting and CNC services. View our full lineup of products here.</p>
+        <p>We specialize in lead and lead-based products such as Sheet Lead and Lead Bricks for radiation shielding, Lead Anodes for electroplating and Lead Sash Weights for counter balances. We also offer our Reliance brand of sacrificial anodes​ and custom die casting and CNC services. View our full lineup of products <a href='/products'>here.</a></p>
         <p>Our technicians are knowledgeable in all aspects of metalwork, from design and pouring to metal finishing and coating. We can help you find a solution to your metal needs.</p>
       </div>
       <h1>Our Brands and Products</h1>
