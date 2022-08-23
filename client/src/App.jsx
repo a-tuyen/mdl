a2ad8f4f2cc72ef8fd3ae822c40e2e14240c66ee
Amanda Tuyen <atuyen26@gmail.com>
2022-08-23 15:03:30 -0700

    added favicon

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,14 @@ import Nav from './components/Nav';
 import Footer from './components/Footer';
 import LeadAnodes from './routes/LeadAnodes';
 import { Outlet } from 'react-router-dom';
+import Favicon from "react-favicon";
 
 
 
 const App =() => {
   return (
       <div className="App">
+         <Favicon url="https://user-images.githubusercontent.com/77664153/179275176-1d25fcfe-6d5e-478d-a1b2-2b461e6da938.png"></Favicon>
         <Nav/>
         <Outlet />
         <Footer />
