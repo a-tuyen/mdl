eecb49b58d56327f30d11bee6aec7883f80197ac
Amanda Tuyen <atuyen26@gmail.com>
2022-09-01 15:52:07 -0700

    working on order form contents and styling

--- client/src/routes/Order.jsx
+++ client/src/routes/Order.jsx
@@ -5,111 +5,133 @@ import '../styles/ContactForm.scss'
 
 const Order = () => {
 
-    const [status, setStatus] = useState("SUBMIT");
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-      setStatus("SENDING...");
-      const { name, company, email, message } = e.target.elements;
-      let details = {
-        name: name.value,
-        company: company.value,
-        email: email.value,
-        message: message.value,
-      };
-      let response = await fetch("http://localhost:5000/contact", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json;charset=utf-8",
-        },
-        body: JSON.stringify(details),
-      });
-      setStatus("SUBMIT");
-      let result = await response.json();
-      alert(result.status);
+  const [status, setStatus] = useState("SUBMIT");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setStatus("SENDING...");
+    const { name, company, email, message } = e.target.elements;
+    let details = {
+      name: name.value,
+      company: company.value,
+      email: email.value,
+      message: message.value,
     };
-    
+    let response = await fetch("http://localhost:5000/contact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(details),
+    });
+    setStatus("SUBMIT");
+    let result = await response.json();
+    alert(result.status);
+  };
 
-    return (
-        <div className="order">
-            <h1> Place An Order</h1>
-            <form onSubmit={handleSubmit}>
-      {/* <div>
-        <label htmlFor="name">Name:</label>
-        <input type="text" id="name" required />
-      </div>
-      <div>
-        <label htmlFor="company">Company:</label>
-        <input type="text" id="company" />
-      </div>
-      <div>
-        <label htmlFor="phone">Phone #:</label>
-        <input type="phone" id="phone" required />
-      </div>
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" required />
-      </div>
-      <div>
-        <label htmlFor="thickness">Thickness:</label>
-        <input type="number" id="thickness" required />
-      </div>
-      <div>
-        <label htmlFor="Width">Width:</label>
-        <input type="number" id="width" required />
-      </div>
-      <div>
-        <label htmlFor="thickness">Length:</label>
-        <input type="number" id="length" required />
-      </div>
-      <div>
-        <label htmlFor="message">Message:</label>
-        <textarea id="message" required />
-      </div> */}
 
-<div>
-        {/* <label placeholder="name">Name:</label> */}
-        <input type="text" placeholder="Name" id="name" required />
-      </div>
-      <div>
-        {/* <label placeholder="company">Company:</label> */}
-        <input type="text" placeholder="Company" id="company" />
-      </div>
-      <div>
-        {/* <label placeholder="phone">Phone #:</label> */}
-        <input type="phone" placeholder="Phone" id="phone" required />
-      </div>
-      <div>
-        {/* <label placeholder="email">Email:</label> */}
-        <input type="email" placeholder="Email" id="email" required />
-      </div>
-      <div>
-        {/* <label placeholder="thickness">Thickness:</label> */}
-        <input type="number" placeholder="Thickness" id="thickness" required />
-      </div>
-      <div>
-        {/* <label placeholder="Width">Width:</label> */}
-        <input type="number" placeholder="Width" id="width" required />
-      </div>
-      <div>
-        {/* <label placeholder="thickness">Length:</label> */}
-        <input type="number" placeholder="Length" id="length" required />
-      </div>
-      <div>
-        {/* <label placeholder="thickness">Length:</label> */}
-        <input type="number" placeholder="Quantity" id="quantity" required />
-      </div>
-      <div>
-        {/* <label placeholder="thickness">Length:</label> */}
-        <input type="text" placeholder="Date Required" id="date-required" required />
-      </div>
-      <div>
-        {/* <label placeholder="message">Message:</label> */}
-        <textarea id="message" placeholder="Message" />
-      </div>
-      <button type="submit">{status}</button>
-    </form>
+  return (
+    <div className="order">
+      <h1> Place An Order</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" required />
         </div>
-    );
+        <div>
+          <label htmlFor="company">Company:</label>
+          <input type="text" id="company" />
+        </div>
+        <div>
+          <label htmlFor="phone">Phone #:</label>
+          <input type="phone" id="phone" required />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" required />
+        </div>
+        {/* <fieldset> */}
+          <div>
+            <label htmlFor="thickness">Thickness:</label>
+            <select name="thickness" id="thickness">
+              <option value='1#'>1#/ft² - 1/64" - 0.4mm</option>
+              <option value='2#'>2#/ft² - 1/32" - 0.8mm</option>
+              <option value='3#'>3#/ft² - 3/64" - 1.2mm</option>
+              <option value='4#'>4#/ft² - 1/16" - 1.6mm</option>
+              <option value='5#'>5#/ft² - 5/64" - 2mm</option>
+              <option value='6#'>6#/ft² - 3/32" - 2.4mm</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="Width">Width:</label>
+            <input type="number" id="width" required />
+            <label>inches</label>
+          </div>
+          <div>
+            <label htmlFor="thickness">Length:</label>
+            <input type="number" id="length" required />
+            <label>inches</label>
+          </div>
+          <div>
+            <label htmlFor="custom">Custom Size:</label>
+            <input id="custom" />
+            <label>(thickness x length x width)</label>
+          </div>
+        {/* </fieldset> */}
+        <div>
+          <label htmlFor="notes">Additional Notes:</label>
+          <textarea id="notes" required />
+        </div>
+
+
+        {/* <div>
+          <label placeholder="company">Company:</label>
+          <input type="text" placeholder="Company" id="company" />
+        </div>
+        <div>
+          <label placeholder="phone">Phone #:</label>
+          <input type="phone" placeholder="Phone" id="phone" required />
+        </div>
+        <div>
+          <label placeholder="email">Email:</label>
+          <input type="email" placeholder="Email" id="email" required />
+        </div>
+        <div>
+          <label placeholder="thickness">Thickness:</label>
+          <input list="thickness" placeholder="Thickness" required />
+          <datalist id="thickness">
+            <option value='1#/ft² - 1/64" - 0.4mm' />
+            <option value='2#/ft² - 1/32" - 0.8mm' />
+            <option value='3#/ft² - 3/64" - 1.2mm' />
+            <option value='4#/ft² - 1/16" - 1.6mm' />
+            <option value='5#/ft² - 5/64" - 2mm' />
+            <option value='6#/ft² - 3/32" - 2.4mm' />
+            <option value='6#/ft² - 3/32" - 2.4mm' />
+          </datalist>
+        </div>
+        <div>
+          <label placeholder="Width">Width:</label>
+          <input type="number" placeholder="Width" id="width" required />
+        </div>
+        <div>
+          <label placeholder="thickness">Length:</label>
+          <input type="number" placeholder="Length" id="length" required />
+        </div>
+        <div>
+          <label placeholder="thickness">Length:</label>
+          <input type="number" placeholder="Quantity" id="quantity" required />
+        </div>
+        <div>
+          <label placeholder="thickness">Length:</label>
+          <input type="text" placeholder="Date Required" id="date-required" required />
+        </div>
+        <div>
+          <label placeholder="message">Message:</label>
+          <textarea id="message" placeholder="Message" />
+        </div> */}
+        <button type="submit">{status}</button>
+      </form>
+    </div>
+  );
 };
 
 export default Order;
