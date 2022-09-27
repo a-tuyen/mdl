710a368832b9e82322914680fff20d2a5dd31bb7
Amanda Tuyen <atuyen26@gmail.com>
2022-09-27 15:34:43 -0700

    remove functions in form

--- client/src/components/ContactForm.jsx
+++ client/src/components/ContactForm.jsx
@@ -4,32 +4,32 @@ import '../styles/ContactForm.scss';
 
 const ContactForm = () => {
 
-  const navigate = useNavigate();
-  const [status, setStatus] = useState("SUBMIT");
+  // const navigate = useNavigate();
+  // const [status, setStatus] = useState("SUBMIT");
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setStatus("SENDING...");
-    const { name, company, phone, email, message } = e.target.elements;
-    let details = {
-      name: name.value,
-      company: company.value,
-      phone: phone.value,
-      email: email.value,
-      message: message.value,
-    };
-    let response = await fetch("http://localhost:5000/contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify(details),
-    });
-    setStatus("SUBMIT");
-    let result = await response.json();
-    navigate('/contact/thankyou');
-    // alert(result.status);
-  };
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   setStatus("SENDING...");
+  //   const { name, company, phone, email, message } = e.target.elements;
+  //   let details = {
+  //     name: name.value,
+  //     company: company.value,
+  //     phone: phone.value,
+  //     email: email.value,
+  //     message: message.value,
+  //   };
+  //   let response = await fetch("http://localhost:5000/contact", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json;charset=utf-8",
+  //     },
+  //     body: JSON.stringify(details),
+  //   });
+  //   setStatus("SUBMIT");
+  //   let result = await response.json();
+  //   navigate('/contact/thankyou');
+  //   // alert(result.status);
+  // };
   
   return (
     <form name="contact" method="POST" data-netlify="true" onSubmit="submit">
