0ecfdc93cc8fbb9f3f108fb15f45bb97695e0b0d
Amanda Tuyen <atuyen26@gmail.com>
2022-09-28 10:44:22 -0700

    add back in  all contact fields

--- client/src/components/ContactForm.jsx
+++ client/src/components/ContactForm.jsx
@@ -38,31 +38,29 @@ const ContactForm = () => {
   // };
   
   return (
-    // <form name="contact" method="POST" data-netlify="true" onSubmit="submit">
+
     <form name="contact" method="post">
     <input type="hidden" name="form-name" value="contact" />
-
       <div>
         <label htmlFor="name">Name:</label>
         <input type="text" name="name"/>
       </div>
-      {/* <div>
+      <div>
         <label htmlFor="company">Company:</label>
         <input type="text" id="company" />
-      </div> */}
-      {/* <div>
+      </div>
+      <div>
         <label htmlFor="phone">Phone #:</label>
         <input type="tel" id="phone" required />
-      </div> */}
+      </div>
       <div>
         <label htmlFor="email">Email:</label>
         <input type="email" name="email"/>
       </div>
-
-      {/* <div>
+      <div>
         <label htmlFor="message">Message:</label>
         <textarea id="message" required />
-      </div> */}
+      </div>
       <button type="submit">Send</button>
     </form>
   );
