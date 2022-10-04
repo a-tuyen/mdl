c8adb6415c77768bba7c2afb26cd78602cd1a6f4
Amanda Tuyen <atuyen26@gmail.com>
2022-10-04 13:10:58 -0700

    minor changes to mobile nav styling

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,8 +1,10 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import '../styles/Nav.scss';
 
 
 const Nav = () => {
+    const [showFullMenu, setShowFullMenu] = useState(true)
     return (
         <>
             <div className='Nav'>
@@ -10,22 +12,22 @@ const Nav = () => {
                     <a href='/'><img src='https://user-images.githubusercontent.com/77664153/179275176-1d25fcfe-6d5e-478d-a1b2-2b461e6da938.png' alt='Metal Distributors Logo'></img></a>
                     <a href='/'><h1 className='title'>METAL DISTRIBUTORS LTD</h1></a>
                 </div>
-                <div className='buttonMenu'>
-                    <Link to ='/home'><p className='button1'>Home</p></Link>
-                    <Link to ='/about'><p className='button1'>About Us</p></Link>
+                <div className='buttonMenu' id={showFullMenu ? 'hidden' : ''}>
+                    <Link to='/home'><p className='button1'>Home</p></Link>
+                    <Link to='/about'><p className='button1'>About Us</p></Link>
                     <div className='product-menu'>
-                        <Link to ='/products'><p className='button1'>Products</p></Link>
+                        <Link to='/products'><p className='button1'>Products</p></Link>
                         <div className="dropdown-content">
-                            <Link to ='/products/lead'><p className='drop-btn'>Lead</p></Link>
-                            <Link to ='/products/sacrificialanodes'><p className='drop-btn'>Sacrificial Anodes</p></Link>
-                            <Link to ='/products/cnc'><p className='drop-btn'>CNC Machining</p></Link>
+                            <Link to='/products/lead'><p className='drop-btn'>Lead</p></Link>
+                            <Link to='/products/sacrificialanodes'><p className='drop-btn'>Sacrificial Anodes</p></Link>
+                            <Link to='/products/cnc'><p className='drop-btn'>CNC Machining</p></Link>
                         </div>
-                    </div> 
-                    <Link to ='/contact'><p className='button1'>Contact Us</p></Link>
+                    </div>
+                    <Link to='/contact'><p className='button1'>Contact Us</p></Link>
                 </div>
-                <button className='burger-menu'><img src='https://user-images.githubusercontent.com/77664153/193146639-29a09769-c716-471b-9b48-48b825cfc856.png'></img></button>
+                <button className='burger-menu' onClick={() => setShowFullMenu(!showFullMenu)}><img src='https://user-images.githubusercontent.com/77664153/193863578-19407891-0c7f-4a86-be70-62e013e4b9c5.png'></img></button>
             </div>
-        
+
             <hr></hr>
 
         </>
