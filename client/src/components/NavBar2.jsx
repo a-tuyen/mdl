4767d059df46f8aa7f6a2b4c8233795c22968222
Amanda Tuyen <atuyen26@gmail.com>
2022-10-13 11:15:16 -0700

    deactivated unused Nav files by changing name and removing links

---
+++ client/src/components/NavBar2.jsx
@@ -0,0 +1,14 @@
+// import Nav from './Nav'
+// import NavLinks from './NavLinks';
+// import NavMobile from './NavMobile'
+
+const NavBar = () => {
+    return (
+        <div>
+            {/* <Nav />
+            <NavMobile /> */}
+        </div>
+    )
+};
+
+export default NavBar;
---
+++ client/src/components/NavMobile2.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+// import Burger from './Burger';
+// import '../styles/Nav.scss';
+// import NavLinks from './NavLinks';
+
+const NavMobile = () => {
+
+const [open, setOpen] = useState(false)
+
+    return (
+        <div className='NavMobile'>
+            {/* <NavLinks /> */}
+            {/* <Burger onClick={() => console.log('this has been clicked!')}/> */}
+        </div>
+    )
+};
+
+export default NavMobile;
