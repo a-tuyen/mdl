adf4bc8a3a594ee77e70dea4ab8fd4822602785c
Amanda Tuyen <atuyen26@gmail.com>
2022-10-14 14:43:56 -0700

    minor change to aboutus, working on footer to list items

--- client/src/components/Footer2.jsx
+++ client/src/components/Footer2.jsx
@@ -5,6 +5,24 @@ const Footer = () => {
         <div className='footer2'>
             {/* <hr></hr> */}
 
+            {/* <section className='side-section'>
+                <h2>WE ARE HERE TO HELP</h2>
+                <div className='info'>
+                    <img src="https://user-images.githubusercontent.com/77664153/189227458-6398c5cc-22ab-47ea-8149-cba5ba7d6f23.png" alt='rotary telephone icon' />
+                    <p>1 (877) 420 3731</p>
+                </div>
+                <div className='info'>
+                    <img src="https://user-images.githubusercontent.com/77664153/189203263-d8200e4d-2aa4-4b03-b138-bc12c7e758f9.png" alt='globe icon' />
+                    <p>info@metaldist.com</p>
+                </div>
+                <div className='info'>
+                    <img className='map-icon' src="https://user-images.githubusercontent.com/77664153/189387060-cabc9d81-b64d-466d-bf56-d5233f03e7fa.png" alt='map with pin drop' />
+                    <p>7220 Winston Street, Burnaby, BC  V5A 2G9</p>
+                </div>
+                <p className='copyright'>Copyright © 2022 Metal Distributors LTD</p>
+
+            </section>           */}
+
             <section className='side-section'>
                 <h2>WE ARE HERE TO HELP</h2>
                 <div className='info'>
@@ -21,7 +39,16 @@ const Footer = () => {
                 </div>
                 <p className='copyright'>Copyright © 2022 Metal Distributors LTD</p>
 
-            </section>          
+                <ul >
+                    <li className='testing'> <img src="https://user-images.githubusercontent.com/77664153/189227458-6398c5cc-22ab-47ea-8149-cba5ba7d6f23.png" alt='rotary telephone icon' />
+                    <p>1 (877) 420 3731</p></li>
+
+                <li>                    <img src="https://user-images.githubusercontent.com/77664153/189203263-d8200e4d-2aa4-4b03-b138-bc12c7e758f9.png" alt='globe icon' />
+                    <p>info@metaldist.com</p></li>
+                </ul>
+
+            </section>
+
 
             <div className='submit'><button className='order'><a href='/order'>Place an order</a></button></div>
         </div>
