2a8b48afb472d9ae2644a0c5aedd93305d76536a
Amanda Tuyen <atuyen26@gmail.com>
2022-11-16 13:46:00 -0800

    fixes styling of about page

--- client/src/routes/About.jsx
+++ client/src/routes/About.jsx
@@ -4,8 +4,7 @@ import '../styles/About.scss';
 const About = () => {
   return (
     <div className="about">
-      <div className='main-section'>
-        <div className='text'>
+            <div className='text'>
           <h1>About Us</h1>
           <p>Established in 1949, Metal Distributors has over 70 years of experience in the lead industry.  We pride ourselves on producing quality products and source our raw materials from only reputable sources.</p>
           <p>All our products are locally made in Burnaby, BC, by our team of skilled technicians. </p>
@@ -13,7 +12,7 @@ const About = () => {
         </div>
         <img className='robot' src='https://user-images.githubusercontent.com/77664153/185462150-c89b5ca0-150f-401f-833d-d9201def1c99.jpg' alt='robot made from anodes' />
       </div>
-    </div>
+    // </div>
   );
 };
 
