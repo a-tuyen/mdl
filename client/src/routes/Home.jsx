059f94bbe55098617b2627ffda44ab543c16d7aa
Amanda Tuyen <atuyen26@gmail.com>
2022-12-08 10:24:48 -0800

    edit home page main img size and add in mobile version

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -7,7 +7,12 @@ const Home = () => {
   return (
     <div className='Home'>
       <div className='paragraph'>
+        <div className='web-size'>
         <img src='https://user-images.githubusercontent.com/77664153/180872927-70cd5e4c-b534-49ad-83ce-3ac92fdd11fc.png' alt='rolls of sheet lead on a shelf'></img>
+        </div>
+        <div className='mobile-size'>
+        <img src='https://user-images.githubusercontent.com/77664153/206293919-06da80d3-df4d-4e3a-b40c-3b8c039a04d8.png'></img>
+        </div>
         <p>​​Metal Distributors is a leading non-ferrous metal manufacturer and precision machining company serving a variety of different industries including the marine, nuclear, plumbing, construction and medical industries.</p>
         <p>We specialize in lead and lead-based products such as Sheet Lead and Lead Bricks for radiation shielding, Lead Anodes for electroplating and Lead Sash Weights for counter balances. We also offer our Reliance brand of sacrificial anodes​ and custom die casting and CNC services. View our full lineup of products <a href='/products'>here.</a></p>
         <p>Our technicians are knowledgeable in all aspects of metalwork, from design and pouring to metal finishing and coating. We can help you find a solution to your metal needs.</p>
