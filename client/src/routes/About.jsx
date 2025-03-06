fc2cf859b7aa9c1f5531974c422fc14be09e0a56
Amanda Tuyen <atuyen26@gmail.com>
2025-03-06 11:46:19 -0800

    changes about us page pic and updates years of experience

--- client/src/routes/About.jsx
+++ client/src/routes/About.jsx
@@ -6,11 +6,11 @@ const About = () => {
     <div className="about">
             <div className='text'>
           <h1>About Us</h1>
-          <p>Established in 1949, Metal Distributors has over 70 years of experience in the lead industry.  We pride ourselves on producing quality products and source our raw materials from only reputable sources.</p>
+          <p>Established in 1949, Metal Distributors has over 75 years of experience in the lead industry.  We pride ourselves on producing quality products and source our raw materials from only reputable sources.</p>
           <p>All our products are locally made in Burnaby, BC, by our team of skilled technicians. </p>
           <p>We are a proud member of the North American Die Casting Association.</p>
         </div>
-        <img className='robot' src='https://user-images.githubusercontent.com/77664153/185462150-c89b5ca0-150f-401f-833d-d9201def1c99.jpg' alt='robot made from anodes' />
+        <img className='robot' src='/MDL-badge.jpg' alt='robot made from anodes' />
       </div>
     // </div>
   );
